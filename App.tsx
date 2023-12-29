9a5d9fe370d07cc39430c5c88742be05c3485fff
Victor Eduardo <83921750+DoyteDoyenr@users.noreply.github.com>
2023-12-29 13:46:40 -0300

    Feat:QrCodeScanner (#1)
    * Initial commit
    
    Generated by create-expo-app 2.1.1.
    
    * feat:QrCode

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import Home from './src/Screens/Home/Index';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <Home />
+    </>
+  );
+}
